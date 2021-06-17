64e409edb21a238e86bd788a413c6fe6ee4b4580
FehPM <fernandop09@hotmail.com>
2021-06-17 11:28:16 -0300

    heroku

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
 
 function App() {
+  const [input, setInput] = useState()
+  const [result, setResult] = useState("Select a number")
+
+  const isValidNumber = () => {
+    if (isNaN(input)) {
+      alert("Input a valid value => NUMBER")
+      return false
+    } else {
+      if (input < 0) {
+        alert("Input a valid value => POSITIVE NUMBER")
+        return false
+      }
+      return true
+    }
+  }
+
+  const fetchSum = async () => {
+    if (!isValidNumber())
+      return
+    const res = await fetch("https://ikfibos.herokuapp.com/sum/" + input);
+    const data = await res.json();
+    setResult(data["Fibonacci_sum"]);
+  }
+
+  const fetchFibo = async () => {
+    if (!isValidNumber())
+      return
+    const res = await fetch("https://ikfibos.herokuapp.com/fibonacci/" + input);
+    const data = await res.json();
+    setResult(data["Fibonacci"]);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+      <h1>Fibonacci calculator</h1>
+      <input className="input" type={"number"} onChange={e => setInput(parseInt(e.target.value))}/>
+      <div className="buttons">
+        <div className="fetch-button" onClick={fetchFibo}>Fibonacci</div>
+        <div className="fetch-button" onClick={fetchSum}>Sum Fibonacci</div>
+      </div>
+      <h2>{result}</h2>
     </div>
   );
 }
